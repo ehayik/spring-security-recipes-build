6503e925bca68fc2a4316df99b6203466bf96640
eduardo.eljaiek <eduardo.eljaiek@gmail.com>
2018-05-09 15:05:40 -0400

    [+] Spring Security Recipes Starter

---
+++ spring-security-recipes-starter/src/main/java/org/github/eljaiek/spring/security/recipes/starter/SecurityRecipesAutoConfiguration.java
@@ -0,0 +1,8 @@
+package org.github.eljaiek.spring.security.recipes.starter;
+
+import org.github.eljaiek.security.core.SecurityRecipesConfigurer;
+import org.springframework.context.annotation.Import;
+
+@Import(SecurityRecipesConfigurer.class)
+public class SecurityRecipesAutoConfiguration {
+}
